c1c6c2473d3c0fc852ed689ec70a9ac8b01d571f
user@example.com <user@example.com>
2022-11-03 10:59:54 +0100

    Update DistribuisciEsamiGUI.csproj, Program.cs, and upgrade-assistant.clef

--- DistribuisciEsamiGUI/Program.cs
+++ DistribuisciEsamiGUI/Program.cs
@@ -12,6 +12,7 @@ namespace DistribuisciEsamiGUI
         private static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
